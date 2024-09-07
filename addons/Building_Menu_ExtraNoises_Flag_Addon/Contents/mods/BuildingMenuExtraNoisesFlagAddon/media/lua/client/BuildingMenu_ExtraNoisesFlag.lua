695b0d6f2277a70ff2288dfd9db4db9d88e0642d
krisbidev <32956577+krisbidev@users.noreply.github.com>
2024-09-07 11:10:32 +0200

    Add sandbox options to toggle each subcategory

--- addons/Building_Menu_ExtraNoises_Flag_Addon/Contents/mods/BuildingMenuExtraNoisesFlagAddon/media/lua/client/BuildingMenu_ExtraNoisesFlag.lua
+++ addons/Building_Menu_ExtraNoises_Flag_Addon/Contents/mods/BuildingMenuExtraNoisesFlagAddon/media/lua/client/BuildingMenu_ExtraNoisesFlag.lua
@@ -71,12 +71,86 @@ local function addExtraNoisesFlagsToMenu()
         )
     end
 
+    --[[
     local flagCountCategories = {
         ["local"] = { usa = 16, mixed = 16 },
         misc = { random = 32, org = 16, pride = 16, reddit = 32, fictional = 32, protest = 16, colors = 16 },
         national = { mideast = 16, africa = 32, native = 16, oceania = 16, asia = 16, historical = 32, mixed = 16, movements = 32, europe = 32, americas = 16 },
         regional = { usa = 56, canada = 16, europe = 16 }
     }
+    ]]--
+    local flagCountCategories = {
+        ["local"] = {},
+        ["misc"] = {},
+        ["national"] = {},
+        ["regional"] = {}
+    }
+    if SandboxVars.BuildingMenu.extraNoiseFlagLocalUSA then
+        flagCountCategories["local"].usa = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagLocalMixed then
+        flagCountCategories["local"].mixed = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscRandom then
+        flagCountCategories.misc.random = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscOrg then
+        flagCountCategories.misc.org = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscPride then
+        flagCountCategories.misc.pride = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscReddit then
+        flagCountCategories.misc.reddit = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscFictional then
+        flagCountCategories.misc.fictional = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscProtest then
+        flagCountCategories.misc.protest = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagMiscColors then
+        flagCountCategories.misc.colors = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalMideast then
+        flagCountCategories.national.mideast = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalAfrica then
+        flagCountCategories.national.africa = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalNative then
+        flagCountCategories.national.native = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalOceania then
+        flagCountCategories.national.oceania = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalAsia then
+        flagCountCategories.national.asia = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalHistorical then
+        flagCountCategories.national.historical = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalMixed then
+        flagCountCategories.national.mixed = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalMovements then
+        flagCountCategories.national.movements = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalEurope then
+        flagCountCategories.national.europe = 32
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagNationalAmericas then
+        flagCountCategories.national.americas = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagRegionalUSA then
+        flagCountCategories.regional.usa = 56
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagRegionalCanada then
+        flagCountCategories.regional.canada = 16
+    end
+    if SandboxVars.BuildingMenu.extraNoiseFlagRegionalEurope then
+        flagCountCategories.regional.europe = 16
+    end
 
     for category, subCategories in pairs(flagCountCategories) do
         for subCategory, count in pairs(subCategories) do
@@ -102,4 +176,5 @@ end
 local function addCategoriesToBuildingMenu()
     addExtraNoisesFlagsToMenu()
 end
+
 Events.OnGameStart.Add(addCategoriesToBuildingMenu)
